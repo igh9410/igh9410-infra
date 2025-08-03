3ddecb74f661668d793f6c3c47a617813cd6ef00
Geon Hyuk Im <athanasia9410@gmail.com>
2025-08-03 11:18:36 +0900

    feat: add Loki helm chart into ArgoCD

---
+++ k8s/terraform/secrets.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_secret" "cloudflare_r2_credentials" {
+  metadata {
+    name      = "cloudflare-r2-credentials"
+    namespace = "monitoring"
+  }
+  data = {
+    # Ensure the key content does not have extra newlines before/after the BEGIN/END markers
+    # The provider will base64 encode this string data.
+     "ACCESS_KEY_ID" = var.r2_access_key_id
+     "ACCESS_SECRET_KEY" = var.r2_secret_access_key
+  }  
+}
