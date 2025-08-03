649f8c6f6b35403abd0bc466c8fb02b3273b38b2
Geon Hyuk Im <athanasia9410@gmail.com>
2025-08-03 12:58:21 +0900

    feat: merge to main

--- k8s/terraform/loki.tf
+++ k8s/terraform/loki.tf
@@ -3,4 +3,18 @@ resource "cloudflare_r2_bucket" "loki_chunk_bucket" {
   name          = "igh9410-loki-chunk"
   location      = "apac"
   storage_class = "Standard"
+}
+
+resource "cloudflare_r2_bucket" "loki_ruler_bucket" {
+  account_id    = var.cloudflare_account_id
+  name          = "igh9410-loki-ruler"
+  location      = "apac"
+  storage_class = "Standard"
+}
+
+resource "cloudflare_r2_bucket" "loki_admin_bucket" {
+  account_id    = var.cloudflare_account_id
+  name          = "igh9410-loki-admin"
+  location      = "apac"
+  storage_class = "Standard"
 } 
--- k8s/terraform/secrets.tf
+++ k8s/terraform/secrets.tf
@@ -6,7 +6,7 @@ resource "kubernetes_secret" "cloudflare_r2_credentials" {
   data = {
     # Ensure the key content does not have extra newlines before/after the BEGIN/END markers
     # The provider will base64 encode this string data.
-     "ACCESS_KEY_ID" = var.r2_access_key_id
-     "ACCESS_SECRET_KEY" = var.r2_secret_access_key
-  }  
+    "ACCESS_KEY_ID"     = var.r2_access_key_id
+    "ACCESS_SECRET_KEY" = var.r2_secret_access_key
+  }
 }
