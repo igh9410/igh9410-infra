76fff84898ab60015fc5e1b31bc2895c5a878b22
Geon Hyuk Im <athanasia9410@gmail.com>
2025-09-25 11:56:53 +0900

    feat: update dns config

--- gramnuri/terraform/dev/cloudflare.tf
+++ gramnuri/terraform/dev/cloudflare.tf
@@ -6,7 +6,7 @@ provider "cloudflare" {
 resource "cloudflare_dns_record" "dev_api" {
   zone_id = var.cloudflare_zone_id
   name    = "dev-api.gramnuri.com"
-  content = "3975cdcd-ffa2-462d-8a88-202402a706ab.cfargotunnel.com"
+  content = "ff8eab80-66b1-44ec-a485-94558ae3dc39.cfargotunnel.com"
   type    = "CNAME"
   ttl     = 1    # Auto TTL
   proxied = true # Set to false if you don't want to use Cloudflare's proxy
@@ -24,14 +24,6 @@ resource "cloudflare_dns_record" "web" {
   proxied = true # Keep proxied enabled for Cloudflare benefits
 }
 
-# TikTok Developer Site Verification TXT Record
-resource "cloudflare_dns_record" "tiktok_verification" {
-  zone_id = var.cloudflare_zone_id
-  name    = "dev.gramnuri.com"
-  type    = "TXT"
-  content = "tiktok-developers-site-verification=uuYK4VKuqEC5wSbmq1klvqViJEiml8IC"
-  ttl     = 1 # Automatic TTL
-}
 
 resource "cloudflare_r2_bucket" "gramnuri_r2_bucket" {
   account_id    = var.cloudflare_account_id
