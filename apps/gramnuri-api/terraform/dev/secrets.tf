db77f01549d2fe9c1823a61750d0bddf868d097e
Geon Hyuk Im <athanasia9410@gmail.com>
2025-10-08 17:35:13 +0900

    feat: add GEMINI API KEY

--- apps/gramnuri-api/terraform/dev/secrets.tf
+++ apps/gramnuri-api/terraform/dev/secrets.tf
@@ -17,9 +17,6 @@ resource "kubernetes_secret" "gramnuri_secrets" {
     FIREBASE_CLIENT_CERT_URL     = var.firebase_client_cert_url
     ENVIRONMENT                  = var.environment
     CLIENT_DOMAIN                = var.client_domain
-    TIKTOK_CLIENT_KEY            = var.tiktok_client_key
-    TIKTOK_CLIENT_SECRET         = var.tiktok_client_secret
-    TIKTOK_REDIRECT_URI          = var.tiktok_redirect_uri
     CLOUDFLARE_ACCOUNT_ID        = var.cloudflare_account_id
     CLOUDFLARE_ACCESS_KEY_ID     = var.cloudflare_access_key_id
     CLOUDFLARE_SECRET_ACCESS_KEY = var.cloudflare_secret_access_key
@@ -27,5 +24,6 @@ resource "kubernetes_secret" "gramnuri_secrets" {
     AWS_ACCESS_KEY_ID            = var.aws_access_key_id
     AWS_SECRET_ACCESS_KEY        = var.aws_secret_access_key
     R2_CUSTOM_DOMAIN             = var.r2_custom_domain
+    GEMINI_API_KEY               = var.gemini_api_key
   }
 }
