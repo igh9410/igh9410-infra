634fb3014cfb2e7cf6db151d019053e4a2b6a905
Geon Hyuk Im <athanasia9410@gmail.com>
2025-10-10 02:51:43 +0900

    feat: ready to merge

--- infrastructure/terraform/cilium.tf
+++ infrastructure/terraform/cilium.tf
@@ -1,4 +1,4 @@
-resource "helm_release" "cilium" {
+/* resource "helm_release" "cilium" {
   name       = "cilium"
   repository = "https://helm.cilium.io/"
   chart      = "cilium"
@@ -8,4 +8,4 @@ resource "helm_release" "cilium" {
   values = [
     file("values/cilium.yaml")
   ]
-}
+} */
