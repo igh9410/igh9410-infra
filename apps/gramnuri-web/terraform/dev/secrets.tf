7e7fc63ff4ba991182231b5a9ce7edc144e91448
Geon Hyuk Im <athanasia9410@gmail.com>
2025-10-26 20:00:18 +0900

    hotfix: security

--- apps/gramnuri-web/terraform/dev/secrets.tf
+++ apps/gramnuri-web/terraform/dev/secrets.tf
@@ -11,14 +11,14 @@ resource "kubernetes_secret" "gramnuri_client_secrets" {
   type = "Opaque"
 
   data = {
-    VITE_FIREBASE_API_KEY = var.firebase_api_key
-    VITE_FIREBASE_AUTH_DOMAIN = var.firebase_auth_domain
-    VITE_FIREBASE_PROJECT_ID = var.firebase_project_id
-    VITE_FIREBASE_STORAGE_BUCKET = var.firebase_storage_bucket
-    VITE_FIREBASE_MESSAGING_SENDER_ID = var.firebase_messaging_sender_id
-    VITE_FIREBASE_APP_ID = var.firebase_app_id
-    VITE_FIREBASE_MEASUREMENT_ID = var.firebase_measurement_id
-    VITE_BACKEND_API_URL = var.backend_api_url
-    VITE_INTERNAL_API_URL = var.internal_api_url
+    FIREBASE_API_KEY = var.firebase_api_key
+    FIREBASE_AUTH_DOMAIN = var.firebase_auth_domain
+    FIREBASE_PROJECT_ID = var.firebase_project_id
+    FIREBASE_STORAGE_BUCKET = var.firebase_storage_bucket
+    FIREBASE_MESSAGING_SENDER_ID = var.firebase_messaging_sender_id
+    FIREBASE_APP_ID = var.firebase_app_id
+    FIREBASE_MEASUREMENT_ID = var.firebase_measurement_id
+    BACKEND_API_URL = var.backend_api_url
+    INTERNAL_API_URL = var.internal_api_url
   }
 }
